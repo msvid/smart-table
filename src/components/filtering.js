6c7b0a8d4f39c718ae4133332e2fbbf525c0c746
Marta Svid <martasvid@yandex.ru>
2025-11-23 20:42:53 +0300

    fix

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,30 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {                        // используйте name как значение и текстовое содержимое
+                        const optionTag = createElement('option')
+                        optionTag.textContent = name
+                        optionTag.value = name                                // @todo: создать и вернуть тег опции
+                        return optionTag
+                    })
+        )
+     })
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
+        if(action && action.name == 'clear') {
+            const wraper = action.closest('.filter-wrapper');
+            wraper.querySelector('input').value = '';
+        };
 
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
