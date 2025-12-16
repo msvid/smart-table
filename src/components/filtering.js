265ecf46c6c991169b4e542f70c8a9a309aafda5
Marta Svid <martasvid@yandex.ru>
2025-12-16 14:26:12 +0300

    feat: 7 спринт

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,30 +1,43 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
 const compare = createComparison(defaultRules);
 
-export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)                                    // Получаем ключи из объекта
-      .forEach((elementName) => {                        // Перебираем по именам
-        elements[elementName].append(                    // в каждый элемент добавляем опции
-            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
-                      .map(name => {                        // используйте name как значение и текстовое содержимое
-                        const optionTag = document.createElement('option')
-                        optionTag.textContent = name
-                        optionTag.value = name                                // @todo: создать и вернуть тег опции
-                        return optionTag
-                    })
-        )
-     })
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }))
+        })
+    }
+
+    const applyFiltering = (query, state, action) => {
+        // код с обработкой очистки поля
         if(action && action.name == 'clear') {
             const wraper = action.closest('.filter-wrapper');
             wraper.querySelector('input').value = '';
-        };
+        }; 
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+        // @todo: #4.5 — отфильтровать данные, используя компаратор
+        data.filter(row => compare(row, state));
+
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+                }
+            }
+        })
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
     }
-}
+
+    return {
+        updateIndexes,
+        applyFiltering
+    }
+} 
--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import {initFiltering} from "./components/filtering.js"
 import {initSearching} from "./components/searching.js"
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -38,15 +38,17 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
+    let query = {}; // копируем для последующего изменения
     // @todo: использование
-    result = applySearching(result, state, action);
-    result = applyFiltering(result, state, action);
-    result = applySorting(result, state, action);
-    result = applyPagination(result, state, action);
-    sampleTable.render(result)
+    query = applySearching(query, state, action);;
+    query = applyFiltering(query, state, action);
+    query = applySorting(query, state, action);;
+    query = applyPagination(query, state, action);
+    const { total, items } = await api.getRecords(query);
+    updatePagination(total, query);
+    sampleTable.render(items)
 }
 
 const sampleTable = initTable({
@@ -60,9 +62,9 @@ const sampleTable = initTable({
 
 const applySearching = initSearching('search');
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {    // передаём элементы фильтра
-    searchBySeller: indexes.sellers                                    // для элемента с именем searchBySeller устанавливаем массив продавцов
-});
+const {applyFiltering, updateIndexes} = initFiltering(
+    sampleTable.filter.elements,
+);                                   // для элемента с именем searchBySeller устанавливаем массив продавцов);
 
 const applySorting = initSorting([        // Нам нужно передать сюда массив элементов, которые вызывают сортировку, чтобы изменять их визуальное представление
     sampleTable.header.elements.sortByDate,
@@ -72,20 +74,22 @@ const applySorting = initSorting([        // Нам нужно передать 
 
 
 
-const applyPagination = initPagination(
-    sampleTable.pagination.elements,             // передаём сюда элементы пагинации, найденные в шаблоне
-    (el, page, isCurrent) => {                    // и колбэк, чтобы заполнять кнопки страниц данными
-        const input = el.querySelector('input');
-        const label = el.querySelector('span');
-        input.value = page;
-        input.checked = isCurrent;
-        label.textContent = page;
-        return el;
-    }
+const {applyPagination, updatePagination} = initPagination(
+    sampleTable.pagination.elements,
+    createPage
 );
+    
 
 const appRoot = document.querySelector('#app');
 
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+   const indexes = await api.getIndexes() 
+
+   updateIndexes(sampleTable.filter.elements, {
+        searchBySeller: indexes.sellers
+   });
+} 
+
+init().then(render);
